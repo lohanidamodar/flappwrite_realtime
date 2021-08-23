ee97206a2646c0525e45846dae19c1cd93d93374
Damodar Lohani <dlohani48@gmail.com>
2021-08-23 15:00:47 +0545

    realtime events

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:appwrite/appwrite.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,6 +28,78 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<Map<String, dynamic>> items = [];
   TextEditingController _nameController = TextEditingController();
+  RealtimeSubscription? subscription;
+  late final Client client;
+  final itemsCollection = '61236158b75b6';
+  late final Database database;
+
+  @override
+  void initState() {
+    super.initState();
+    client = Client()
+            .setEndpoint('https://dlrealtime.appwrite.org/v1') // your endpoint
+            .setProject('61236151f0744') //your project id
+        ;
+    database = Database(client);
+    login();
+    loadItems();
+    subscribe();
+  }
+
+  login() async {
+    try {
+      await Account(client)
+          .createSession(email: 'test@testme.com', password: 'testtest');
+    } on AppwriteException catch (e) {
+      print(e.message);
+    }
+  }
+
+  loadItems() async {
+    try {
+      final res = await database.listDocuments(collectionId: itemsCollection);
+      setState(() {
+        items = List<Map<String, dynamic>>.from(res.data['documents']);
+      });
+    } on AppwriteException catch (e) {
+      print(e.message);
+    }
+  }
+
+  void subscribe() {
+    final realtime = Realtime(client);
+
+    subscription = realtime.subscribe([
+      'collections.$itemsCollection.documents'
+    ]); //replace <collectionId> with the ID of your items collection, which can be found in your collection's settings page.
+
+    // listen to changes
+    subscription!.stream.listen((data) {
+      // data will consist of `event` and a `payload`
+      if (data["payload"] != null) {
+        switch (data["event"]) {
+          case "database.documents.create":
+            var item = data['payload'];
+            items.add(item);
+            setState(() {});
+            break;
+          case "database.documents.delete":
+            var item = data['payload'];
+            items.removeWhere((it) => it['\$id'] == item['\$id']);
+            setState(() {});
+            break;
+          default:
+            break;
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    subscription?.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +110,15 @@ class _HomePageState extends State<HomePage> {
       body: ListView(children: [
         ...items.map((item) => ListTile(
               title: Text(item['name']),
+              trailing: IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () async {
+                  await database.deleteDocument(
+                    collectionId: itemsCollection,
+                    documentId: item['\$id'],
+                  );
+                },
+              ),
             )),
       ]),
       floatingActionButton: FloatingActionButton(
@@ -75,9 +157,15 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  void _addItem(String name) {
-    setState(() {
-      items.add({'name': name, 'id': DateTime.now().millisecondsSinceEpoch});
-    });
+  void _addItem(String name) async {
+    try {
+      await database.createDocument(
+          collectionId: itemsCollection,
+          data: {'name': name},
+          read: ['*'],
+          write: ['*']);
+    } on AppwriteException catch (e) {
+      print(e.message);
+    }
   }
 }
