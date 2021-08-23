8768c42aa853ece220bbbb8320d4c681b9384f8c
Damodar Lohani <dlohani48@gmail.com>
2021-08-23 14:15:28 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/example/flappwrite_realtime/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flappwrite_realtime
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlAppwrite Realtime Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> items = [];
+  TextEditingController _nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FlAppwrite Realtime Demo'),
+      ),
+      body: ListView(children: [
+        ...items.map((item) => ListTile(
+              title: Text(item['name']),
+            )),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          // dialog to add new item
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text('Add new item'),
+              content: TextField(
+                controller: _nameController,
+              ),
+              actions: [
+                TextButton(
+                  child: Text('Cancel'),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+                TextButton(
+                  child: Text('Add'),
+                  onPressed: () {
+                    // add new item
+                    final name = _nameController.text;
+                    if (name.isNotEmpty) {
+                      _nameController.clear();
+                      _addItem(name);
+                    }
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void _addItem(String name) {
+    setState(() {
+      items.add({'name': name, 'id': DateTime.now().millisecondsSinceEpoch});
+    });
+  }
+}
